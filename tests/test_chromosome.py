acc7ba3aa107632087c4e3c3ab4f87214820b4df
S.Mohammad Mousavi <smmousavisp@gmail.com>
2020-03-20 08:10:31 +0330

    Add test for general_crossover

--- tests/test_chromosome.py
+++ tests/test_chromosome.py
@@ -33,3 +33,16 @@ def test_phenotype_get_x_and_y():
     c = get_from_gen_list_chromosome(gen_list)
     assert c.get_x() == 5
     assert c.get_y() == -5
+
+
+def test_general_crossover():
+    c = get_from_gen_list_chromosome([1 for x in range(4)])
+    d = get_from_gen_list_chromosome([0 for x in range(4)])
+
+    first_child, second_child = c.general_crossover(d, [0, 2, 4])
+    assert str(first_child) == "1100"
+    assert str(second_child) == "0011"
+
+    first_child, second_child = c.general_crossover(d, [0, 1, 2, 3, 4])
+    assert str(first_child) == "1010"
+    assert str(second_child) == "0101"
