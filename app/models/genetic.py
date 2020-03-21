70525d8802e1c62964cb35f9d05dc909f2ad08d4
S.Mohammad Mousavi <smmousavisp@gmail.com>
2020-03-21 08:32:53 +0430

    Add __init__ for Genetic class

--- app/models/genetic.py
+++ app/models/genetic.py
@@ -1,3 +1,25 @@
 """
 Genetic algorithm implementation for Ackley function.
 """
+
+
+class Genetic:
+    """ Genetic algorithm for Ackley function.
+
+    """
+
+    def __init__(
+            self,
+            chromosome_size,
+            population_size,
+            generation_count,
+            crossover_method="3_point",
+            parent_selection_method="rws",
+            survival_selection_method="rws"
+    ):
+        self.chromosome_size = chromosome_size
+        self.population_size = population_size
+        self.generation_count = generation_count
+        self.crossover_method = crossover_method
+        self.parent_selection_method = parent_selection_method
+        self.survival_selection_method = survival_selection_method
