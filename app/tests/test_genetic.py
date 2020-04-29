118d4af8261839f866084239c31d37e3679f0f5f
S.Mohammad Mousavi <smmousavisp@gmail.com>
2020-04-29 16:12:53 +0430

    Change test_initialize_population to test_genetic

--- app/tests/test_genetic.py
+++ app/tests/test_genetic.py
@@ -2,9 +2,8 @@ from models.genetic import Genetic
 from models.chromosome import Chromosome
 
 
-def test_initialize_population():
+def test_genetic():
     g = Genetic(10, 10, 10)
-    g.initialize_population()
     assert len(g.current_generation) == 10
     assert isinstance(g.current_generation[0], Chromosome)
     assert g.current_generation[0].size == 10
