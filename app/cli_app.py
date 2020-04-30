23077fc545f451d89a8128d49a9705d88fff4696
S.Mohammad Mousavi <smmousavisp@gmail.com>
2020-04-30 16:31:10 +0430

    Get data from input for running genetic algorithm

--- app/cli_app.py
+++ app/cli_app.py
@@ -17,6 +17,8 @@ def main():
 
     generation_count = int(input("Enter number of generations:"))
 
+    genetic = Genetic(chromosome_size, population_size, generation_count)
+
     print("Enter crossover method:")
     print("1. n_point")
     print("2. single_point")
@@ -24,12 +26,41 @@ def main():
     crossover_method = ""
     crossover_method_number = int(input())
     if crossover_method_number == 1:
-        crossover_method = int(input("Enter n:")) + "_point"
+        crossover_method = input("Enter n:") + "_point"
     elif crossover_method_number == 2:
         crossover_method = "single_point"
     elif crossover_method == 3:
         crossover_method = "uniform"
-    genetic = Genetic(chromosome_size, population_size, generation_count)
+
+    parent_selection_method = selection_input("parent selection")
+
+    survival_selection_method = selection_input("survival selection")
+
+    genetic.run(
+        crossover_method,
+        parent_selection_method,
+        survival_selection_method
+    )
+
+
+def selection_input(selection_type):
+    """ Get selection method from input
+
+    :param selection_type: Type of selection (parent selection or
+    survival selection)
+    :type selection_type: str
+    """
+    print(f"Enter {selection_type} method:")
+    print("1. Roulette Wheal Selection (RWS)")
+    print("2. Stochastic Universal Sampling (SUS)")
+    print("3. Tournament Selection (TS)")
+    selection_number = int(input())
+    if selection_number == 1:
+        return "rws"
+    elif selection_number == 2:
+        return "sus"
+    elif selection_number == 3:
+        return "ts_" + input("Enter size of tournament:")
 
 
 if __name__ == "__main__":
