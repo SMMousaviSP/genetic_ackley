07d6365bb3fd0313a93e64af9075311370222f49
S.Mohammad Mousavi <smmousavisp@gmail.com>
2020-04-30 14:19:08 +0430

    Add cli_app.py for cli entry point

---
+++ app/cli_app.py
@@ -0,0 +1,13 @@
+"""
+Command-line interface for genetic algorithm implementation for ackley
+function.
+"""
+
+
+def main():
+    """ Main function
+    """
+
+
+if __name__ == "__main__":
+    main()
